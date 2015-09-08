a97352a6209f9218b1fbc5224d88a76d0796226f
nickstumpos <nick.stumpos@gmail.com>
2015-09-08 17:04:37 -0500

    Update NickStumpos.java

--- NickStumpos.java
+++ NickStumpos.java
@@ -4,20 +4,18 @@ import robocode.*;
 import robocode.util.Utils;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
-
 /**
  * NickStumpos - a robot by (your name here)
  */
 public class NickStumpos extends AdvancedRobot {
-	private static double WALLDIST = 50;
+	private static double WALLDIST = 100;
 
 	private class Body {
 		public Body(double xForce, double yForce) {
@@ -36,55 +34,46 @@ public class NickStumpos extends AdvancedRobot {
 		public double getyForce() {
 			return yForce;
 		}
-
 	}
 
 	private static class Enemy {
 		private List<Double> bearings = new ArrayList<Double>();
-		private List<Double> xS = new ArrayList<Double>();
-		private List<Double> yS = new ArrayList<Double>();
+		private List<Point2D> locs = new ArrayList<Point2D>();
 		private List<Double> energy = new ArrayList<Double>();
 		private List<Double> heading = new ArrayList<Double>();
 		private List<Double> distance = new ArrayList<Double>();
 		private List<Double> velocity = new ArrayList<Double>();
-		private double last;
+		private long last;
 
-		Enemy(double bearing, double energy, double distance, double heading, double velocity, double time, double x,
-				double y) {
+		Enemy(double bearing, double energy, double distance, double heading,
+				double velocity, long time, Point2D pos) {
 			this.bearings.add(bearing);
 			this.energy.add(energy);
 			this.heading.add(heading);
 			this.distance.add(distance);
 			this.velocity.add(velocity);
-			this.xS.add(x);
-			this.yS.add(y);
+			this.locs.add(pos);
 			this.last = time;
 		}
 
-		public void addStats(double bearing, double energy, double distance, double heading, double velocity,
-				double time, double x, double y) {
+		public void addStats(double bearing, double energy, double distance,
+				double heading, double velocity, long time, Point2D pos) {
 			this.bearings.add(bearing);
 			this.energy.add(energy);
 			this.heading.add(heading);
 			this.distance.add(distance);
 			this.velocity.add(velocity);
-			this.xS.add(x);
-			this.yS.add(y);
+			this.locs.add(pos);
 			this.last = time;
 		}
 
-		public double findLastBearing() {
-			return bearings.get(bearings.size() - 1);
+
+
+		public Point2D findLastPos() {
+			return locs.get(locs.size() - 1);
 		}
 
-		public double findLastX() {
-			return xS.get(xS.size() - 1);
-		}
-
-		public double findLastY() {
-			return yS.get(yS.size() - 1);
-		}
-
+		
 		public double findLastEnergy() {
 			return energy.get(energy.size() - 1);
 		}
@@ -101,158 +90,124 @@ public class NickStumpos extends AdvancedRobot {
 			return velocity.get(velocity.size() - 1);
 		}
 
-		public double getLastTimeSeen() {
+		public long getLastTimeSeen() {
 			return this.last;
 		}
 	}
 
 	private Map<String, Enemy> enemies = new HashMap<String, Enemy>();
 	private Enemy currentTarget;
-	
-	private Enemy findCurrentEnemyToTarget() {
-		if (!enemies.isEmpty()) {
-			currentTarget = enemies.values().iterator().next();
-		}
-		return currentTarget;
-	}
 
-	private void doMove(boolean ignoreEnemies) {
+	private void doMove() {
 		double xForce = 0;
 		double yForce = 0;
+		Point2D me = new Point2D.Double(getX(),getY());
 		if (getBattleFieldWidth() - WALLDIST < getX()) {
 			// force from closest point on GETy,WIDTH
-			Body exertingBody = findForcesFromBody(getBattleFieldWidth(), getX(), getY(), getY());
+			Body exertingBody = findForcesFromBody(new Point2D.Double(getBattleFieldWidth(),getY()), me);
 			xForce -= exertingBody.getXForce();
 			yForce -= exertingBody.getyForce();
-		} 
+		}
 		if (0 + WALLDIST > getX()) {
 			// force from closest point on getY,0
-			Body exertingBody = findForcesFromBody(0, getX(), getY(), getY());
+			Body exertingBody = findForcesFromBody(new Point2D.Double(0, getY()), me);
 			xForce -= exertingBody.getXForce();
 			yForce -= exertingBody.getyForce();
 		}
-		if (getBattleFieldHeight() - WALLDIST > getY()) {
+		if (getBattleFieldHeight() - WALLDIST < getY()) {
 			// force from closest point on getY,Height
-			Body exertingBody = findForcesFromBody(getX(), getX(), getBattleFieldHeight(), getY());
+			Body exertingBody = findForcesFromBody(new Point2D.Double(getX(),getBattleFieldHeight()), me);
 			xForce -= exertingBody.getXForce();
 			yForce -= exertingBody.getyForce();
-		} 
+		}
 		if (0 + WALLDIST > getY()) {
 			// force from closest point on getY,Width
-			Body exertingBody = findForcesFromBody(getX(), getX(), 0, getY());
+			Body exertingBody = findForcesFromBody(new Point2D.Double(getX(),0), me);
 			xForce -= exertingBody.getXForce();
 			yForce -= exertingBody.getyForce();
 		}
-		if (!ignoreEnemies) {
-			for (Enemy e : enemies.values()) {
-				Body exertingBody = findForcesFromBody(e.findLastX(), getX(), e.findLastY(), getY());
-				xForce -= exertingBody.getXForce();
-				yForce -= exertingBody.getyForce();
-			}
+		for (Enemy e : enemies.values()) {
+			Body exertingBody = findForcesFromBody(e.findLastPos(), me);
+			xForce -= exertingBody.getXForce();
+			yForce -= exertingBody.getyForce();
 		}
-		double angle = Math.atan2(xForce, yForce);
+		
+		double theta = Math.atan2(xForce, yForce);
 		if (xForce == 0 && yForce == 0) {
 			// If no force, do nothing
 		} else {
-			double absRads=angle - getHeadingRadians();
-			double finalHeading = Utils.normalRelativeAngle(absRads);
-			setTurnRightRadians(finalHeading);
-			
-			double interceptRadians=Math.PI/2-absRads;
+			double relativeHeading = Utils.normalRelativeAngle(theta - getHeadingRadians());
+			if (!Utils.isNear(0, relativeHeading)) {
+				turnRightRadians(relativeHeading);
+			}
 			double dist;
-			dist = computeDistanceToMove(absRads, interceptRadians);
+			boolean goingForward = Math.abs(theta - getHeadingRadians()) < Math.PI / 2;
+			dist = computeDistanceToMove(theta,goingForward);
 			if (xForce == 0 && yForce == 0) {
-			    // If no force, do nothing
-			} else if(Math.abs(angle-getHeadingRadians())<Math.PI/2){
-			    setAhead(dist-WALLDIST);
+				System.out.println("Not Moving");
+			} else if (goingForward) {
+				ahead(dist - WALLDIST);
+				System.out.println("Moving: "+(dist - WALLDIST));
+				
 			} else {
-			    setAhead(-1*(dist-WALLDIST));
+				ahead(-1 * (dist - WALLDIST));
+				System.out.println("Moving: "+(-1 * (dist - WALLDIST)));
 			}
 		}
-	}
-
-	private double computeDistanceToMove(double absRads, double interceptRadians) {
-		double dist;
-		if(0==absRads||absRads<Math.PI*2){
-			dist=getBattleFieldWidth()-getX();
-			
-		}else if(0<absRads&&absRads<=Math.PI/4){
-			dist = getBattleFieldWidth()-getX()/Math.sin(interceptRadians);
-		}else if(Math.PI/4<absRads&&absRads<Math.PI/2){
-			dist = getBattleFieldHeight()-getY()/Math.sin(interceptRadians);				
-		}else if(absRads==Math.PI/2){
-			dist = getBattleFieldHeight()-getY();
-		}else if(Math.PI/2<absRads&&absRads<=3*Math.PI/4){
-			dist = getBattleFieldHeight()-getY()/Math.sin(interceptRadians);
-			
-		}else if(3*Math.PI/4<absRads&&absRads<Math.PI){
-			dist = getX()/Math.sin(interceptRadians);
-			
-		}else if(Math.PI==absRads){
-			dist = getX();				
-		}else if(Math.PI<absRads&&absRads<=5*Math.PI/4){
-			dist = getX()/Math.sin(interceptRadians);
-			
-		}else if(5*Math.PI/4<absRads&&absRads<=3*Math.PI/2){
-			dist = getY()/Math.sin(interceptRadians);
-			
-		}else if(3*Math.PI/2==absRads){
-			dist = getY();				
-		}else{
-			dist = getY()/Math.sin(interceptRadians);
+		while(true){
+			double distremaining =getDistanceRemaining();
+			execute();
+			if(distremaining==0)break;
 		}
-		return dist;
+		
 	}
 
-	private Body findForcesFromBody(double bodyX, double myX, double bodyY, double myY) {
-		double absBearing = Utils.normalAbsoluteAngle(Math.atan2(bodyX - myX, bodyY - myY));
-		double dist = distanceFromPoint(bodyX, myX, bodyY, myY);
-		Body exertingBody = new Body(Math.sin(absBearing) / Math.pow(dist, 2),
-				Math.cos(absBearing) / Math.pow(dist, 2));
+	private double computeDistanceToMove(double theta) {
+		double dist;
+		theta=Math.abs(Utils.normalAbsoluteAngle(theta));
+		if (Utils.isNear(0,theta) || Utils.isNear(theta, Math.PI * 2) ) {
+			dist = getBattleFieldWidth() - getX();
+		} else if (Utils.isNear(Math.PI/2,theta)) {
+			dist =getBattleFieldHeight()-getY();
+		}  else if (Utils.isNear(Math.PI, theta)) {
+			dist = getX();
+		} else if (Utils.isNear(3*Math.PI/2, theta)) {
+			dist = getY();
+		} else if (0 < theta && theta <= Math.PI / 4) {
+			dist = (getBattleFieldWidth() - getX()) / Math.sin(Math.PI / 2 - theta);
+		} else if (Math.PI / 4 < theta && theta < Math.PI / 2) {
+			dist = (getBattleFieldWidth() - getX()) / Math.sin(theta);
+		} else if (Math.PI / 2 < theta && theta <= 2*Math.PI / 3) {
+			dist = (getX()) / Math.sin(theta);
+		} else if (2*Math.PI / 3 < theta && theta < Math.PI) {
+			dist = (getX()) / Math.sin(theta);
+		} else if (Math.PI  < theta && theta < 5*Math.PI/4) {
+			dist = (getX()) / Math.sin(theta);
+		} 
+		return Math.abs(dist);
+	}
+
+	private Body findForcesFromBody(Point2D me, Point2D body) {
+		double deltaX = body.getX() - me.getX();
+		double deltaY = body.getY() - me.getY();		
+		double absoluteAngle = Utils.normalAbsoluteAngle(Math.atan2(deltaX,deltaY));
+		double distSq = me.distanceSq(body);
+		Body exertingBody = new Body(Math.sin(absoluteAngle) / distSq, Math.cos(absoluteAngle) / distSq);
 		return exertingBody;
 	}
-	private double distanceFromPoint(double bodyX, double myX, double bodyY, double myY) {
-		double ysquare = Math.pow(bodyY - myY, 2);
-		double xsquare = Math.pow(bodyX - myX, 2);
-		double dist = Math.sqrt(xsquare + ysquare);
-		return dist;
-	}
 
-	double normalizeBearing(double angle) {
-		while (angle > 180)
-			angle -= 360;
-		while (angle < -180)
-			angle += 360;
-		return angle;
-	}
-
+	int direction = 1;
+	
 	/**
 	 * run: NickStumpos's default behavior
 	 */
 	public void run() {
-
 		setColors(Color.red, Color.blue, Color.green); // body,gun,radar
-
-		// Robot main loop
-		while (true) {
-			Enemy e = findCurrentEnemyToTarget();
-			if (e != null) {
-				Double lastBearing = e.bearings.get(e.bearings.size() - 1);
-
-				turnGunRight(normalizeBearing(getHeading() - getGunHeading() + lastBearing));
-				while (getGunTurnRemaining() > 0) {
-					execute();
-				}
-				fire(3);
-				doMove(false);
-			}
-			turnRadarLeft(360);
-
-			while (getDistanceRemaining() > 0 && getRadarTurnRemaining() > 0) {
-				execute();
-			}
-
-		}
+		setAdjustRadarForGunTurn(true);
+		setAdjustGunForRobotTurn(true);
+		do {
+			turnRadarRightRadians(Double.POSITIVE_INFINITY); 
+		} while (true);
 	}
 
 	/**
@@ -261,30 +216,34 @@ public class NickStumpos extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double absBearing = e.getBearingRadians() + getHeadingRadians();
 		if (!enemies.containsKey(e.getName())) {
-
-			enemies.put(e.getName(),
-					new Enemy(e.getBearing(), e.getEnergy(), e.getDistance(), e.getHeading(), e.getVelocity(),
-							getTime(), getX() + e.getDistance() * Math.sin(absBearing),
-							getY() + e.getDistance() * Math.cos(absBearing)));
+			enemies.put(
+					e.getName(),
+					new Enemy(e.getBearingRadians(), e.getEnergy(), e.getDistance(), e
+							.getHeading(), e.getVelocity(), getTime(), new Point2D.Double(
+							+ e.getDistance() * Math.sin(absBearing), getY()
+							+ e.getDistance() * Math.cos(absBearing))));
 		} else {
-			enemies.get(e.getName()).addStats(e.getBearing(), e.getEnergy(), e.getDistance(), e.getHeading(),
-					e.getVelocity(), getTime(), getX() + e.getDistance() * Math.sin(absBearing),
-					getY() + e.getDistance() * Math.cos(absBearing));
+			enemies.get(e.getName()).addStats(e.getBearingRadians(), e.getEnergy(),
+					e.getDistance(), e.getHeading(), e.getVelocity(),
+					getTime(), new Point2D.Double(getX() + e.getDistance() * Math.sin(absBearing),
+					getY() + e.getDistance() * Math.cos(absBearing)));
 		}
+		setTurnRadarRightRadians(Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians()) * 2);
+		doMove();
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		doMove(false);
+		doMove();
 	}
 
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		doMove(true);
+		doMove();
 	}
 
 	@Override
